311dc8435ad6a41ffd2e8881e43d2a26715969fe
Artur <info@aigroupusa.com>
2021-02-12 10:56:35 -0500

    commit tree f585bb59cbfcc31f0cbd2e9874809a8d5c691243

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a <= 0 || b <= 0 || c <= 0 || a+b<=c || b+c<=a || a+c<=b
+      
+        #begin
+          raise TriangleError
+        #rescue TriangleError => error
+        #puts error.message
+        #ßend
+      
+    elsif a == b && b == c
+      :equilateral
+    elsif (a == b && b != c) || (a == c && c != b)  || (b == c && c != a)
+      :isosceles
+    elsif a != b && a != c && b != c
+      :scalene
+    end 
+  end
+  
+  class TriangleError < StandardError
+    def message
+       "Invalid triangle!"
+    end
+  end
+
+
 end
